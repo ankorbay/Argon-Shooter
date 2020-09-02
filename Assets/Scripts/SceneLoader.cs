c0a66a6c3d9f1332568ebad1eb831a431d6d6535
Anton Korshunov <anton.korshunov@banuba.com>
2020-09-02 19:05:52 +0300

    common commit to save all the work

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextScene",2f);
+    }   
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
