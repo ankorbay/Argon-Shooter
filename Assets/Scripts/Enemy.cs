1226cbd7fa6a22af9b16d66ccfb2d8fc309c100d
Anton Korshunov <anton.korshunov@banuba.com>
2020-09-04 17:15:47 +0300

    Score, particle collisions, music singleton, enemy fx are added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+    void Start()
+    {
+        addNonTriggerBoxCollider();
+    }
+
+    private void addNonTriggerBoxCollider(){
+        Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
+    }
+    void OnParticleCollision(GameObject other)
+    {   
+        GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
-{
+{   
+
     private void Awake() {
-        DontDestroyOnLoad(gameObject);
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+
+        if(numMusicPlayers > 1){
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
